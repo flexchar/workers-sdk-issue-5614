0db20024ef67f1ff1862b37d941b1433319f5a6a
Lukas <git@lukasnotes.dk>
2024-04-21 22:23:45 +0200

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+    // https://sdk.vercel.ai/docs/guides/frameworks/nuxt
+    import { useChat } from 'ai/vue';
+    import { ref } from 'vue';
+
+    const headers = ref('');
+
+    const { messages, input, handleSubmit } = useChat({
+        api: 'http://localhost:8787/chat',
+        initialInput:
+            'Tell me why compressing streaming responses is a bad idea',
+        onResponse(response) {
+            console.log(response);
+
+            headers.value = JSON.stringify([...response.headers], null, 2);
+
+            // Show compression header
+            alert(response.headers.get('content-encoding'));
+        },
+    });
+</script>
+
+<template>
+    <section class="flex h-full min-h-screen flex-col w-full justify-center">
+        <pre class="text-white">
+          Headers {{ headers }}
+        </pre>
+
+        <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+            <div
+                v-for="m in messages"
+                key="m.id"
+                class="whitespace-pre-wrap text-white"
+            >
+                {{ m.role === 'user' ? 'User: ' : 'AI: ' }}
+                {{ m.content }}
+            </div>
+
+            <form @submit="handleSubmit">
+                <input
+                    class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-white bg-gray-800"
+                    v-model="input"
+                    placeholder="Say something..."
+                    autofocus
+                />
+            </form>
+        </div>
+    </section>
+</template>
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+import { env } from 'hono/adapter';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+    return c.redirect('http://localhost:5173');
+});
+
+app.use(
+    cors({
+        origin: '*',
+        exposeHeaders: ['*'],
+    }),
+);
+
+// https://sdk.vercel.ai/docs/guides/frameworks/nextjs-app
+app.post('/chat', async (c) => {
+    const { OPENAI_API_KEY } = env(c);
+    const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY,
+    });
+
+    const { messages } = await c.req.json();
+
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        stream: true,
+        messages,
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+});
+
+export default app;
